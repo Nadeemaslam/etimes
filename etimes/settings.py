f498d90d4f03a8f7516a313c179d9fa9e3061a2f
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-03-12 22:10:22 +0530

    database setting changes

--- etimes/settings.py
+++ etimes/settings.py
@@ -75,16 +75,22 @@ WSGI_APPLICATION = 'etimes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'etimes_db',
-        'USER': 'admin',
-        'PASSWORD': '',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'etimes_db',
+            'USER': 'admin',
+            'PASSWORD': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
