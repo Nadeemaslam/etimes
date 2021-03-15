0c53788cc04279da5ecc6e509d18b87ae0187b68
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-03-15 11:30:13 +0530

    localhost added in settings

--- etimes/settings.py
+++ etimes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@&96ea8jj8hhu9l@zwtd45d6$hbr_vk0u*85x6o7c)ltrtpw-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['128.199.22.89']
 import os
@@ -83,6 +83,7 @@ DATABASES = {
         'NAME': 'etimes_db',
         'USER': 'admin',
         'PASSWORD': 'bariq@123',
+        'HOST': 'localhost',
     }
 }
 
