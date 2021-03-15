97e6d10a355b4d28fb9bc3625ff49166a6e2f854
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-03-15 11:55:45 +0530

    media_root

--- etimes/settings.py
+++ etimes/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/static/'
 import os
 
 # Add these new lines
@@ -132,4 +133,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
