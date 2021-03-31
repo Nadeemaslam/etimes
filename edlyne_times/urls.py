dcb72f490f678e74a2e38a5cec1238daa45518c7
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-03-31 15:40:53 +0530

    added apis for nse and nasdaq

--- edlyne_times/urls.py
+++ edlyne_times/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'research/', views.research, name='research'),
+    url(r'research/', views.nse, name='nse'),
 ]
