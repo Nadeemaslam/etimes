1e6a2e26b6099d465da67495aff28c4aa54dede3
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-05-18 14:23:17 +0530

    mobile section removed and css added for nav

--- static/edlyne_times/js/scripts.js
+++ static/edlyne_times/js/scripts.js
@@ -20,40 +20,10 @@ window.onscroll = function () {
 
 
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000); // Change image every 3 seconds
-}
 
 
 
-//$("document").ready(function(){
-//  $(".tab-slider--body").hide();
-//  $(".tab-slider--body:first").show();
-//});
-//
-//$(".tab-slider--nav li").click(function() {
-//  $(".tab-slider--body").hide();
-//  var activeTab = $(this).attr("rel");
-//  $("#"+activeTab).fadeIn();
-//  if($(this).attr("rel") == "tab2"){
-//    $('.tab-slider--tabs').addClass('slide');
-//  }else{
-//    $('.tab-slider--tabs').removeClass('slide');
-//  }
-//  $(".tab-slider--nav li").removeClass("active");
-//  $(this).addClass("active");
-//});
+
 
 
 $(function () {
@@ -72,15 +42,6 @@ $(function () {
 
 
 
-//$(document).ready(function() {
-//    $('.dropdown-content').hover(function(){
-//        $('#page-top').addClass('opacity');
-//        console.log("hehehehhehe")
-//    },
-//    function(){
-//        $('#page-top').removeClass('opacity');
-//    });
-//});
 
 
 $('.dropdown').hover(
@@ -229,3 +190,6 @@ $(window).scroll(function() {
   }
 
 });
+
+
+
