ae3654bacd58cd0a049dc4119163963243167b1d
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-07-01 15:01:56 +0530

    minor css changes

--- static/edlyne_times/js/scripts.js
+++ static/edlyne_times/js/scripts.js
@@ -193,3 +193,76 @@ $(window).scroll(function() {
 
 
 
+var a = 0;
+$( window ).on("load", function() {
+
+  var oTop = $('#topcounter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.topcounter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 7000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+  $( ".text-block1" ).show( 1500 );
+  $( ".text-block2" ).show( 1500 );
+
+});
+
+
+$(document).ready(function ($) {
+		function animateElements() {
+			$('.progressbar').each(function () {
+				var elementPos = $(this).offset().top;
+				var topOfWindow = $(window).scrollTop();
+				var percent = $(this).find('.circle').attr('data-percent');
+				var percentage = parseInt(percent, 10) / parseInt(100, 10);
+				var animate = $(this).data('animate');
+				if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+					$(this).data('animate', true);
+					$(this).find('.circle').circleProgress({
+						startAngle: -Math.PI / 2,
+						value: percent / 100,
+						size: 180,
+						thickness: 30,
+						emptyFill: "rgba(0,0,0, .2)",
+						fill: {
+							color: '#fb4f14'
+						}
+					}).on('circle-animation-progress', function (event, progress, stepValue) {
+						$(this).find('div').text((stepValue*100).toFixed(1) + "%");
+					}).stop();
+				}
+			});
+		}
+
+		// Show animated elements
+		animateElements();
+		$(window).scroll(animateElements);
+
+
+
+
+	});
+
+
