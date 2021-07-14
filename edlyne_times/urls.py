0d9e406bce5701fd08630e2ac108fc837b3b88c6
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-07-14 13:42:21 +0530

    url about added

--- edlyne_times/urls.py
+++ edlyne_times/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url('logout/', logoutUser, name='logout'),
     url(r'contact/', contact, name='contact'),
     url(r'^$', views.index, name='index'),
+    url(r'about/', views.about, name='about'),
     url(r'research/(?P<exchange>[\w-]+)/$', views.research, name='research'),
     url(r'reports/(?P<slug>[\w-]+)/$', views.reports, name='reports'),
     url(r'research/', views.nse, name='nse'),
