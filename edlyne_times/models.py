a6b5e6e3bb8338f84ba8ff2197d776febbb54678
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-07-14 13:01:27 +0530

    model field error fixed

--- edlyne_times/models.py
+++ edlyne_times/models.py
@@ -76,7 +76,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
-    content = models.RichTextField()
+    content = RichTextField()
     description = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
