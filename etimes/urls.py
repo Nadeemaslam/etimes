7ab59cac60db051a6330cf323692af85b0688253
Nomaan <nomaanahmad77@gmail.com>
2021-11-26 10:37:38 +0530

    added robots.txt in urls.py

--- etimes/urls.py
+++ etimes/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('edlyne_times.urls')),
     path('accounts/', include('accounts.urls')),
     path('secretpanel/', admin.site.urls),
-    #url(r'^robots\.txt/$', TemplateView.as_view(template_name="etimes/robots.txt", content_type='text/plain')),
+    url(r'^robots\.txt/$', TemplateView.as_view(template_name="etimes/robots.txt", content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
